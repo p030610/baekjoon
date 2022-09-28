b65d27038b876098b580147017ed5a4d1673f0f3
박성재(allen park) <p030610@naver.com>
2022-09-28 21:46:01 +0900

    baekjoon 1065

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 using namespace std;
 
+bool hansu(int n)
+{
+    int array[5] = {-1};
+    
+    int count = 0;
+    
+    while(1)
+    {
+        array[count] = n % 10;
+        n = n / 10;
+        if(n == 0)
+        {
+            break;
+        }
+        count++;
+    }
+    
+    for(int i = 0; i < count; i++)
+    {
+        
+    }
+    return 
+}
+
 int main()
 {
-    print("hello world");
+    
     return 0;
 }
