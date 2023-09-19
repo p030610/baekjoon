2bcc9a39230dddbbaa7d8d5c73f119aa50523a04
박성재(allen park) <p030610@naver.com>
2023-09-19 13:28:32 +0900

    asdf

--- main.cpp
+++ main.cpp
@@ -1,32 +1,29 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
-
-bool hansu(int n)
-{
-    int array[5] = {-1};
-    
-    int count = 0;
-    
-    while(1)
-    {
-        array[count] = n % 10;
-        n = n / 10;
-        if(n == 0)
-        {
-            break;
-        }
-        count++;
-    }
-    
-    for(int i = 0; i < count; i++)
-    {
-        
-    }
-    return 
-}
 
 int main()
 {
+    int n n_for_digit;
+    int b, result;
+    int digit = 0;
+
+    cin >> n >> b;
+
+    n_for_digit = n;
+
+    while(n_for_digit > 0)
+    {
+        n_for_digit /= 10;
+        digit++;
+    }
+
+    while()
+    {
+        result = 
+    }
+
+
     
     return 0;
 }
