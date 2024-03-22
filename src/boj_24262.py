f6e8b5debc18a2a77695d33f5a64e5786552914d
박성재 <p030610@naver.com>
2024-03-22 14:49:53 +0900

    added baekjoon problem

--- src/boj_24262.py
+++ src/boj_24262.py
@@ -1,4 +1,17 @@
-n = input()
+def sum_of_factorials(n):
+    sum = 0
+    for i in range(1,n-1):
+        sum += ((n-(i+1))*(n-(i+1)+1)//2)
+        # for j in range(i+1,n):
+        #     sum = sum + (n+1)-(j+1)
 
-print(int((int(n))*(int(n))*(int(n))))
+        #     print(i,j,n,int((n+1)-(j+1)))
+            # for k in range(j+1,n+1):
+                # sum+=1
+
+    return int(sum)
+
+n = int(input())
+
+print(sum_of_factorials(n))
 print(3)
---
+++ src/boj_24313.py
@@ -0,0 +1,4 @@
+a1,a2 = map(int, input().split())
+c = int(input())
+n0 = int(input())
+
