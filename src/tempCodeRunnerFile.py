1e812723d040aeb1898fb2ba56ee8da6b7629f56
박성재 <p030610@naver.com>
2024-03-25 10:37:54 +0900

    git ignore path modified

--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -1,37 +1,4 @@
+for j in range(i+1,n):
+        #     sum = sum + (n+1)-(j+1)
 
-
-
-def factors(n):
-
-    factors_list = []
-    for i in range(1,n):
-        if n % i == 0:
-            factors_list.append(i)
-
-    sum = 0
-
-    for j in factors_list:
-        sum += int(j)
-
-    if sum == n:
-        default_str = str(n) + " = "
-        for k in factors_list : 
-            default_str = default_str +  str(k) + " + "
-
-        print(default_str[0:-2])
-
-    else:
-        print(str(n) + " is NOT perfect.")
-
-
-n = 0
-            
-while n != -1:
-    n = input()
-
-    n = int(n)
-
-    factors(n)
-
-        
-
+        #     print(i,j,n,int((n+1)-(j+1)))
