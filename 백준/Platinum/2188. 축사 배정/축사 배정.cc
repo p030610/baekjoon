ae9c6e919a07735b47399611e06505c362c444a2
박성재(allen park) <p030610@naver.com>
2025-12-05 00:33:12 +0900

    [Platinum IV] Title: 축사 배정, Time: 4 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Platinum/2188. 축사 배정/축사 배정.cc
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+deque<int> adj[205];
+int origin[205];
+int history[205];
+int ans;
+
+bool try_match(int start){
+    for(int i=0; i<adj[start].size(); i++){
+        if(history[adj[start][i]]){
+            continue;
+        }
+        history[adj[start][i]]=1;
+        if(origin[adj[start][i]]==-1 || try_match(origin[adj[start][i]])){
+            origin[adj[start][i]]=start;
+            
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+int main(){
+    for(int i=0; i<205; i++){
+        origin[i]=-1;
+    }
+    int n,m;
+    scanf("%d %d",&n,&m);
+    for(int i=1; i<=n; i++){
+        int a,b;
+        scanf("%d",&a);
+        for(int j=1; j<=a; j++){
+            scanf("%d",&b);
+            adj[i].push_back(b);
+        }
+    }
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=200; j++){
+            history[j]=0;
+        }
+        if(try_match(i)){
+            ans++;
+        }
+        
+    }
+    printf("%d",ans);
+    return 0;
+}
