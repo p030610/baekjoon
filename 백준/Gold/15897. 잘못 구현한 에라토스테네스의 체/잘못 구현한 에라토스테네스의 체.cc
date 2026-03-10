13c8abe8df71b77fa3c5510e683fcdcef36c576a
박성재(Allen Park) <p030610@naver.com>
2026-03-10 22:45:38 +0900

    [Gold II] Title: 잘못 구현한 에라토스테네스의 체, Time: 4 ms, Memory: 1880 KB -BaekjoonHub

--- 백준/Gold/15897. 잘못 구현한 에라토스테네스의 체/잘못 구현한 에라토스테네스의 체.cc
+++ 백준/Gold/15897. 잘못 구현한 에라토스테네스의 체/잘못 구현한 에라토스테네스의 체.cc
@@ -9,11 +9,12 @@ ll ans;
 ll n;
 
 int main(){
+    m.reserve(1000);
     scanf("%lld",&n);
 //    m[1]={n,n};
     ll prev=n;
     
-    for(int i=1; i<=min(n,(ll)100000); i++){
+    for(int i=1; i<=min(n,(ll)10000); i++){
         ll val=1;
         ll l=1;
         ll r=n;
